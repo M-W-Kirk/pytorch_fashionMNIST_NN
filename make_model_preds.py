fc00508135275ce31f530c90e69acca7c6bd2843
mwkirk <micah.kirkpatrick@iparametrics.com>
2021-11-23 14:08:24 -0500

    updates added

--- make_model_preds.py
+++ make_model_preds.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3
+
+# %%
 import torch
 from fashionMNIST_NN import NeuralNetwork
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
 
+# %%
+# Load the NN model FashionMNIST_Model.pth:
 model = NeuralNetwork()
 model.load_state_dict(torch.load('FashionMNIST_Model.pth'))
 
+# %%
+# Get the FashionMNIST test data:
+test_data = datasets.FashionMNIST(
+    root='data',
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+
+# %%
 # Prediction classes:
 classes = [
     "T-shirt/top",
@@ -26,3 +43,5 @@ with torch.no_ggrad():
     predicted, actual = classes[pred[0].argmax(0)], classes[y]
     print(f'Predicted: "{predicted}", Actual: "{actual}"')
 
+
+# %%
